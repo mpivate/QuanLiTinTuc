d7c9cab334882a3716188cfbfa41d470781af5d3
NguyenNhatMinh <93605710+mpivate@users.noreply.github.com>
2023-12-04 22:53:25 +0700

    update

--- QuanLiTinTuc/Controllers/HomeController.cs
+++ QuanLiTinTuc/Controllers/HomeController.cs
@@ -12,70 +12,19 @@ namespace QuanLiTinTuc.Controllers
         QLTinTuc db = new QLTinTuc();
         public ActionResult Index()
         {
-            return RedirectToAction("TheGioi");
+            return RedirectToAction("ChuDe", new { ChuDe = "TheGioi" });
         }
 
-        //Chủ đề Thế giới
-        public ActionResult TheGioi()
+        //Chủ đề
+        public ActionResult ChuDe(string ChuDe)
         {
-            List<TinTuc> DanhSachTinTuc = db.TinTucs.Where(e => e.ChuDe == "TheGioi").ToList();
+            List<TinTuc> DanhSachTinTuc = db.TinTucs.Where(e => e.ChuDe == ChuDe).ToList();
             return View(DanhSachTinTuc);
         }
-        public ActionResult TheGioiView(int Id)
-        {
-            TinTuc TinThoaMan = db.TinTucs.Where(e => e.Id == Id).FirstOrDefault(); 
-            return View(TinThoaMan);
-        }
-
-        //Chủ đề Kinh Doanh
-        public ActionResult KinhDoanh()
-        {
-            List<TinTuc> DanhSachTinTuc = db.TinTucs.Where(e => e.ChuDe == "KinhDoanh").ToList();
-            return View(DanhSachTinTuc);
-        }
-        public ActionResult KinhDoanhView(int Id)
+        public ActionResult ChuDeView(int Id)
         {
             TinTuc TinThoaMan = db.TinTucs.Where(e => e.Id == Id).FirstOrDefault();
             return View(TinThoaMan);
         }
-
-        //Chủ đề Bất động sản
-        public ActionResult BatDongSan()
-        {
-            List<TinTuc> DanhSachTinTuc = db.TinTucs.Where(e => e.ChuDe == "BatDongSan").ToList();
-            return View(DanhSachTinTuc);
-        }
-        public ActionResult BatDongSanView(int Id)
-        {
-            TinTuc TinThoaMan = db.TinTucs.Where(e => e.Id == Id).FirstOrDefault();
-            return View(TinThoaMan);
-        }
-
-        //Chủ đề khoa học
-        public ActionResult KhoaHoc()
-        {
-            List<TinTuc> DanhSachTinTuc = db.TinTucs.Where(e => e.ChuDe == "KhoaHoc").ToList();
-            return View(DanhSachTinTuc);
-        }
-        public ActionResult KhoaHocView(int Id)
-        {
-            TinTuc TinThoaMan = db.TinTucs.Where(e => e.Id == Id).FirstOrDefault();
-            return View(TinThoaMan);
-        }
-
-        //Chủ đề Giải trí
-        public ActionResult GiaiTri()
-        {
-            List<TinTuc> DanhSachTinTuc = db.TinTucs.Where(e => e.ChuDe == "GiaiTri").ToList();
-            return View(DanhSachTinTuc);
-        }
-        public ActionResult GiaiTriView(int Id)
-        {
-            TinTuc TinThoaMan = db.TinTucs.Where(e => e.Id == Id).FirstOrDefault();
-            return View(TinThoaMan);
-        }
-
-
-
     }
 }
